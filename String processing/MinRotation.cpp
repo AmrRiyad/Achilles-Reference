d5aa32f93b08107d8a504ac03fc76c224eeab831
Amr Riyad <74285642+Achilles404@users.noreply.github.com>
2022-08-25 06:48:37 +0200

    Create MinRotation.cpp

---
+++ String processing/MinRotation.cpp
@@ -0,0 +1,10 @@
+int minRotation(string s) {
+    int a = 0, n = (int) s.size();
+    s += s;
+    for (int b = 0; b < n; b++)
+        for (int k = 0; k < n; k++) {
+            if (a + k == b || s[a + k] < s[b + k]) { b += max(0, k - 1); break; }
+            if (s[a + k] > s[b + k]) { a = b; break; }
+        }
+    return a;
+}
