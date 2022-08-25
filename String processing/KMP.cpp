37e6c4a9665ee69667acb3b8d5cd399249c9ee10
Amr Riyad <74285642+Achilles404@users.noreply.github.com>
2022-08-25 07:08:02 +0200

    Update KMP.cpp

--- String processing/KMP.cpp
+++ String processing/KMP.cpp
@@ -1,10 +1,10 @@
-vector<int> pi;
+vector<int> p;
 string s, t;
 int n, m;
 
 int get(char c, int j) {
     while (j > 0 && c != s[j])
-        j = pi[j - 1];
+        j = p[j - 1];
     if (c == s[j])
         j++;
     return j;
@@ -12,9 +12,9 @@ int get(char c, int j) {
 
 void kmp() {
     for (int i = 1; i < n; i++) {
-        int j = pi[i - 1];
+        int j = p[i - 1];
         j = get(s[i], j);
-        pi[i] = j;
+        p[i] = j;
     }
 }
 
@@ -23,5 +23,5 @@ void Achilles1() {
     s = t + '#' + s;
     n = (int) s.size();
     m = (int) t.size();
-    pi.resize(n);
+    p.resize(n);
 }
