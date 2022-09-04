94d3c3f2d853d186fbbc48695e8f1ef83b3c4fe4
Amr Riyad <74285642+Achilles404@users.noreply.github.com>
2022-09-04 23:19:44 +0200

    Update karp.cpp

--- String processing/karp.cpp
+++ String processing/karp.cpp
@@ -18,3 +18,27 @@ ll popback(ll hash, ll base, int ch) {
     ll x = (hash - ch % M + M) % M;
     return x * power(base, M - 2);
 }
+
+ll h[1000001];
+ll pw[1000001];
+
+ll interval_hash(ll i, ll j) {
+    ll x = (h[j] - h[i - 1] + M) % M; //(1)
+    return x * power(pw[i], M - 2) % M; //(1)
+//  return (h[j] - (ll) h[i - 1] * pw[j - i + 1] + (ll) M*M)%M; // (2)
+}
+
+//  h[0] = 0, pw[0] = 1;
+//  for (int i = 1; i <= n; ++i){
+//      pw[i] = (pw[i - 1] * p) % M;
+//  }
+//
+//  (1)
+//  for (int i = 1; i <= n; ++i) {
+//      h[i] = (h[i - 1] + (s[i - 1] - 'a' + 1) * pw[i - 1] % M) % M;
+//  }
+//
+//  (2)
+//  for (int i = 1; i <= n; ++i) {
+//      h[i] = ((h[i - 1] * p) % M + (s[i - 1] - 'a' + 1) % M) % M;
+//  }
